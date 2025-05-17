f89bce804b057d9530f14d27916b2db349144fb0
LjonesQA <lucasbragajones@gmail.com>
2025-05-17 18:20:58 -0300

    trabalho em andamento

---
+++ trabalho.js
@@ -0,0 +1,21 @@
+function geradorDeTagsDeIdentificacao(nome) {
+  return nome.toUpperCase()
+}
+function verificarSePodeSerAdotado(idade, tamanho ){
+  if (idade===1) {
+    return (tamanho === "M")? "Pode ser adotado"
+    :"Tamanho não permitido pra adoção"   
+  }else{
+    "Infelizmente a idade de adoção tem que ser de 1 ano "
+  }
+  
+}
+function calcularConsumoDeRacao(params) {
+  
+}
+function decidirTipoDeAtividadePorPorte(params) {
+  
+}
+function buscarDadoAsync(params) {
+  
+}
